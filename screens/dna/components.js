1f61f30c2e920099d6c9163b2c5f7705b7e9f9ec
idenaaio <90510246+idenaaio@users.noreply.github.com>
2021-09-13 20:19:11 +0200

    uploading files

---
+++ screens/dna/components.js
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import {Box, Stack, Text, Icon, Link} from '@chakra-ui/core'
+import {useTranslation} from 'react-i18next'
+
+export function DnaDialogStat({label, value, children, ...props}) {
+  return (
+    <Stack
+      isInline
+      spacing={4}
+      align="center"
+      justify="space-between"
+      bg="gray.50"
+      px={5}
+      py={4}
+      {...props}
+    >
+      <Box>
+        {label && <Text color="muted">{label}</Text>}
+        {value && <Text wordBreak="break-all">{value}</Text>}
+      </Box>
+      {children}
+    </Stack>
+  )
+}
+
+export function DnaAppUrl({url}) {
+  const {t} = useTranslation()
+  return (
+    <Stack
+      isInline
+      align="center"
+      spacing={3}
+      color="muted"
+      px={2}
+      py="3/2"
+      mt={16}
+    >
+      <Icon name="laptop" size={5} />
+      <Link href={url}>{t('Open in Idena app')}</Link>
+    </Stack>
+  )
+}
